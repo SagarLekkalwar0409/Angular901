cab3912649dc620798d00b7a945adab2eecac25d
SAGAR <SAGAR@DESKTOP-9M3UD44.lan>
2025-01-27 14:04:56 +0530

    .

--- AngularLoginSignup/src/main/java/com/sl/controller/AuthController.java
+++ AngularLoginSignup/src/main/java/com/sl/controller/AuthController.java
@@ -3,6 +3,7 @@ package com.sl.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import com.sl.service.UserService;
 
 @RestController
 @RequestMapping("/api/auth")
+@CrossOrigin(origins = "http://localhost:4200") 
 public class AuthController {
 
     @Autowired
--- AngularLoginSignup/src/main/java/com/sl/controller/ProfileController.java
+++ AngularLoginSignup/src/main/java/com/sl/controller/ProfileController.java
@@ -2,6 +2,7 @@ package com.sl.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import com.sl.service.ProfileService;
 
 @RestController
 @RequestMapping("/api/profiles")
+@CrossOrigin(origins = "http://localhost:4200") 
 public class ProfileController {
     @Autowired
     private ProfileService profileService;
